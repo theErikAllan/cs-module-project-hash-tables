1d9b9127573b7752c5cdf21a26d42a50fbbe2333
Rick Ahlgren <soul@DESKTOP-MPL9BMJ.attlocal.net>
2020-07-09 18:35:56 -0400

    Completed histo problem

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -3,9 +3,27 @@ import random
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    word_list = words.split()
 
 # TODO: analyze which words can follow other words
 # Your code here
+
+word_dict = {}
+counter = 0
+
+for word in word_list[:-1]:
+    if word not in word_dict:
+        word_dict[word] = []
+    word_dict[word].append(word_list[counter + 1])
+    counter += 1
+
+# separate lists for start and stop words
+# if word starts with " or uppercase
+# if word ends with " or period
+
+# print("Word list: ", word_list)
+print("\n")
+print("Word dictionary: ", word_dict)
 
 
 # TODO: construct 5 random sentences
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -10,7 +10,7 @@ def word_count(s):
         for word in word_list:
             new_word = ""
             for character in word:
-                if character.isalnum():
+                if character.isalpha():
                     new_word += character.lower()
                 elif character == "'":
                     new_word += character
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('":;,.-+=/\\|[]{}()*^&'))
-    # print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    # print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
+    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
